11634fbc79efc0fdd2530a5187f58d717e00f341
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-19 10:46:19 +0200

    TryParse use with a simple case of user input

---
+++ 01_Making_decisions/TryParse/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter a number");
+string input1 = Console.ReadLine();
+
+int num1 = int.Parse(input1);
+Console.WriteLine("User entered number = " + num1);
+// above code will throw an exception if a Parse() can not parse a string to an int
+
+Console.WriteLine("Enter another number");
+string input2 = Console.ReadLine();
+int num2;
+bool isNumber = int.TryParse(input2, out num2);
+if (isNumber )
+{
+    Console.WriteLine("It was a number");
+}
+else
+{
+    Console.WriteLine("It was not a number");
+}
+    Console.WriteLine("User entered number = " + num2);
+
+Console.ReadKey();
