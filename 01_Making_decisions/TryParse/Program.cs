1a0b29d0483d4016fb1d6a91c63d3720a17bf8ea
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-19 11:24:52 +0200

    Random number generation with user input checked by TryParse

--- 01_Making_decisions/TryParse/Program.cs
+++ 01_Making_decisions/TryParse/Program.cs
@@ -1,22 +1,36 @@
-﻿Console.WriteLine("Enter a number");
+﻿/*
+Console.WriteLine("Enter a number");
 string input1 = Console.ReadLine();
-
 int num1 = int.Parse(input1);
 Console.WriteLine("User entered number = " + num1);
 // above code will throw an exception if a Parse() can not parse a string to an int
+*/
 
-Console.WriteLine("Enter another number");
+// instance of the Random Class
+Random random = new Random();
+
+// random number between 1 and 10
+int randomNumber = random.Next(1, 11);
+
+Console.WriteLine("Guess a number");
 string input2 = Console.ReadLine();
 int num2;
 bool isNumber = int.TryParse(input2, out num2);
 if (isNumber )
 {
     Console.WriteLine("It was a number");
+    if (randomNumber == num2)
+    {
+        Console.WriteLine("You guessed a number");
+    }
+    else
+    {
+        Console.WriteLine("You have not guessed the number");
+    }
 }
 else
 {
     Console.WriteLine("It was not a number");
 }
-    Console.WriteLine("User entered number = " + num2);
-
+Console.WriteLine("User entered number = " + num2);
 Console.ReadKey();
