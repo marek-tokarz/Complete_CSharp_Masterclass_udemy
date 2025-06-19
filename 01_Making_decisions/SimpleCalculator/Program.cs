59c304b855953a35fde573b9305a84414bceb963
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-19 12:36:04 +0200

    Added a simple four operations calculator

---
+++ 01_Making_decisions/SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter the first number:");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+int number2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Choose an operation: +, -, *, /");
+string operation = Console.ReadLine();
+char firstChar = operation[0];
+
+int result;
+
+if (firstChar == '+')
+{
+    result = number1 + number2;
+    Console.WriteLine("Result: " + result);
+}
+else if(firstChar == '-')
+{
+    result = number1 - number2;
+    Console.WriteLine("Result: " + result);
+}
+else if(firstChar == '*')
+{
+    result = number1 * number2;
+    Console.WriteLine("Result: " + result);
+}
+else if (firstChar == '/')
+{
+    if (number2 == 0)
+    {
+        Console.WriteLine("Error: Division by zero is not allowed.");
+    }
+    else
+    {
+        result = number1 / number2;
+        Console.WriteLine("Result: " + result);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid operation. Please choose +, -, *, or /.");
+}
+Console.ReadLine();
