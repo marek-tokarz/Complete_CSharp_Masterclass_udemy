d3bf2daecb84037009d9b806b57f88bae42a3528
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-22 15:21:45 +0200

    'break' and 'continue' in a for loop

---
+++ 02_Loops/BreakAndContinue/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("A for loop with break:");
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if(i==3)
+    {
+        Console.WriteLine("Loop finished");
+        break;
+    }
+}
+
+Console.WriteLine("A for loop with continue:");
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        Console.WriteLine("C O N T I N U E");
+        continue;
+    }
+    Console.WriteLine("Only if not 'continue'");
+}
+Console.ReadKey();
