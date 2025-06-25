96241c9a0bf84a0340891fd391a4f7fbf1428725
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-25 18:39:27 +0200

    Simple project to present foreach loop and arrays

---
+++ 02_Loops/ForeachLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿string[] weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+Console.WriteLine("Length of the 'weekdays' array is: " + weekDays.Length);
+
+foreach(var day in weekDays)
+{
+    Console.WriteLine(day);
+}    
+
+// two dimension array
+
+int[,] twoDimensionArray = new int[3, 3];
+// [0] [0] [0]
+// [0] [0] [0]
+// [0] [0] [0]
+
+int[,,] threeDimensionArray = new int[3, 3, 3];
+
+int[,] twoDimensionArrayInitalized = { { 1, 2 }, { 3, 4 } };
+// [1] [2] // row 0
+// [3] [4] // row 1
+//  c0  c1
+
+Console.WriteLine(twoDimensionArrayInitalized[0,0]);
+
+twoDimensionArrayInitalized[0, 0] = 5;
+
+Console.WriteLine(twoDimensionArrayInitalized[0, 0]);
+
+Console.WriteLine(twoDimensionArrayInitalized[1, 0]);
+
+string[,] ticTacToeField =
+{
+    {"O", "X", "X" },
+    {"O", "O", "X" },
+    {"X", "X", "O" },
+};
+
+Console.ReadLine();
