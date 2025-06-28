a221ef1e48bc9b8f015126adec61b0cabda38072
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-28 16:33:53 +0200

    Taking methods outside a Main() method

---
+++ Methods/FieldsInstanceVariables/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FieldsInstanceVariables
+{
+    internal class Program
+    {
+        // Field (or instance variable)
+        static int myResult;
+
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+
+            Console.WriteLine("AddTwoValues method result: " + AddTwoValues(a, b));
+            Console.WriteLine("SubtractTwoValues method result: " + SubtractTwoValues(a, b));
+
+            Console.ReadKey();
+        }
+
+        static int AddTwoValues(int value1, int value2)
+        {
+            myResult = value1 + value2;
+            return myResult;
+        }
+
+        static int SubtractTwoValues(int value1, int value2)
+        {
+            myResult = (value1 - value2);
+            return myResult;
+        }
+    }
+}
