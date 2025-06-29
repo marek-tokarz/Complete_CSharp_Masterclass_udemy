c16232aaefe7c870237d5210df0043d4de9005c9
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-29 16:11:10 +0200

    Multiple constructors - two custom constructors for a class Customer

---
+++ ObjectOrientedProgramming/BasicsOfClassesAndObjects/Customer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfClassesAndObjects
+{
+    internal class Customer
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        // Custom Constructor
+        public Customer(string name, string address, string contacNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contacNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
--- ObjectOrientedProgramming/BasicsOfClassesAndObjects/Program.cs
+++ ObjectOrientedProgramming/BasicsOfClassesAndObjects/Program.cs
@@ -2,22 +2,32 @@
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            /*
             // Creating an object of the Class Car
             // Creating an instance of the Class Car
             Car audi = new Car("A3", "Audi", false);
             Car bmw = new Car("i7", "BMW", true);
 
-            /*
+            
             Console.WriteLine("Enter the brand name");
             // Setting brand of a car - by property
             audi.Brand = Console.ReadLine();
-            */
+            
 
             // Getting a brand of a car - by property
             Console.WriteLine("Brand is " + audi.Brand);
             Console.WriteLine("Brand is " + bmw.Brand);
+            */
+
+            // EXAMPLES WITH CUSTOMER
+
+            Customer earl = new Customer("Earl");
+            Customer marek = new Customer("Marek", "Mainstreet 1", "5551234567");
+
+            Console.WriteLine("Name of a customer is: " + earl.Name);
 
             Console.ReadKey();
         }
