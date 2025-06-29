b2240d1103ddc19a060bbbc89cc73af058b09ae9
marek-tokarz <marek.tokarz14@gmail.com>
2025-06-29 08:28:24 +0200

    Basics of classes and instances of classes, class: Car

---
+++ ObjectOrientedProgramming/BasicsOfClassesAndObjects/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfClassesAndObjects
+{
+    internal class Car
+    {
+        // Constructor
+        public Car() 
+        {
+            Console.WriteLine("An object of car has been created!");
+        }
+    }
+}
---
+++ ObjectOrientedProgramming/BasicsOfClassesAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicsOfClassesAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Console.ReadKey();
+        }
+    }
+}
