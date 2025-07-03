0dc134194eb10a65585a034c734182435f12b571
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-03 16:48:31 +0200

    Calculated property and also read only and write only properties

---
+++ ObjectOrientedProgramming/BasicsOfClassesAndObjects/Rectangle_CalculatedProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsOfClassesAndObjects
+{
+    internal class Rectangle_CalculatedProperty
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        // Computed property
+        public double Area
+        {
+            get {  return Width * Height; }
+        }
+    }
+}
