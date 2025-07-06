05d2c9207330a29781378ed3ea72c3dd2cc9b543
Marek Tokarz <marek.tokarz14@gmail.com>
2025-07-06 16:31:12 +0200

    Final version of question and answers displaying

--- ObjectOrientedProgramming/MyQuizApp/Quiz.cs
+++ ObjectOrientedProgramming/MyQuizApp/Quiz.cs
@@ -17,7 +17,27 @@ namespace MyQuizApp
 
         public void DisplayQuestion(Questions question)
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(
+                "╔═════════════════════════════════════════════════════════════════════════╗"
+            );
+            Console.WriteLine(
+                "║                                 Question                                ║"
+            );
+            Console.WriteLine(
+                "╚═════════════════════════════════════════════════════════════════════════╝"
+            );
+            Console.ResetColor();
             Console.WriteLine(question.QuestionText);
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("  ");
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($". {question.Answers[i]}");
+            }
         }
     }
 }
