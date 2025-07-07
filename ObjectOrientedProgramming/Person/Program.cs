d2266b5f71eb111733c8a982c369fe2d22a324f8
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-07 14:12:17 +0200

    Simple project to practice classes - a class Person with fields, properties, constructor and method

---
+++ ObjectOrientedProgramming/Person/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Person
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("John", 30);
+
+            person.Greet();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Person
+    {
+        private string _name;
+
+        private int _age;
+
+        public string Name {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Hello, my name is {Name} and I am {Age} years old");
+        }
+    }
+}
+
+
