a9dc3a6fac1baca0c043fff55e1a1c17b75db311
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-08 18:05:19 +0200

    Declaring a list, adding items and reading it with a foreach loop

---
+++ Collections/BasicsOfCollections/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BasicsOfCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring and initalizing a List
+            List<string> colors = new List<string>();
+
+            colors.Add("Red");
+            colors.Add("Green");
+            colors.Add("Blue");
+
+            Console.WriteLine("Colors in a list:");
+
+            foreach (string color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
