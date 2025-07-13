80db7c9960c0e1954eae74b0ea2785892b2e5839
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-13 09:30:01 +0200

    ArrayList declaring and using with some methods

--- Collections/BasicsOfCollections/Program.cs
+++ Collections/BasicsOfCollections/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace BasicsOfCollections
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BasicsOfCollections
 {
     public class Product
     {
@@ -88,6 +91,7 @@
             }
             */
 
+            /*
             // Using Lists with Complex Objects
 
             List<Product> products = new List<Product>
@@ -105,6 +109,59 @@
             {
                 Console.WriteLine($"Product name: {product.Name} for {product.Price}");
             }
+
+            // Turning interface I<Enumerable> to a List
+            List<Product> cheapProducts = products.Where(p => p.Price < 1.0).ToList();
+
+            Console.WriteLine("Products for less than 1$:");
+
+            foreach (Product product in cheapProducts)
+            {
+                Console.WriteLine($"Product name: {product.Name} for {product.Price}");
+            }
+            */
+
+            // ArrayList
+
+            // declaring an ArrayList with undefined amount of objects
+            ArrayList myArrayList = new ArrayList();
+
+            // declaring an ArrayList with defined amount of objects
+            ArrayList myArrayList2 = new ArrayList(100);
+
+            myArrayList.Add(25);
+            myArrayList.Add("Hello");
+            myArrayList.Add(13.37);
+            myArrayList.Add(13);
+            myArrayList.Add(21);
+
+            // deletes a specific element
+            myArrayList.Remove(13);
+
+            // deletes an element at a specific position 
+            myArrayList.RemoveAt(0);
+
+            Console.WriteLine(myArrayList.Count);
+
+            double sum = 0;
+
+            foreach(object obj in myArrayList)
+            {
+                if(obj is int)
+                {
+                    sum += Convert.ToDouble(obj); 
+                }
+                else if(obj is double)
+                {
+                    sum += (double)obj;
+                }
+                else if(obj is string)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+
+            Console.WriteLine("Sum is: " + sum);
 
             Console.ReadLine();  
         }
