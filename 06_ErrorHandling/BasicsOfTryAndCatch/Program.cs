cd849a70eae950b39fce540e10911151ea365803
Marek Tokarz <marek.tokarz14@gmail.com>
2025-07-15 15:57:38 +0200

    New project about basics of error handling

---
+++ 06_ErrorHandling/BasicsOfTryAndCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BasicsOfTryAndCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            try
+            {
+                Console.WriteLine("Please enter a number");
+
+                int num1 = 0;
+                int num2 = 2;
+
+                result = num2 / num1;
+                // above line causes exception:
+                // System.DivideByZeroException: „Attempted to divide by zero.”
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
