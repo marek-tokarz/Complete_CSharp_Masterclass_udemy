0cd27541816e8ec87d822c016549fed2c8b5ea04
Marek Tokarz <marek.tokarz14@gmail.com>
2025-07-16 13:16:50 +0200

    Basics of inheritance: base class and derived class

---
+++ 07_Inheritance/BasicsOfInheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicsOfInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Bark();
+            myDog.Eat();
+
+            Console.ReadKey();
+        }
+    }
+
+    // Base class
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    // Derived Class
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Barking...");
+        }
+    }
+}
