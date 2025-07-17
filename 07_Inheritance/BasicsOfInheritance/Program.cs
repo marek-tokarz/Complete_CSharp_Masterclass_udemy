3f12d54137a04695ec4bbd670b17a12d5a34256f
Marek Tokarz <marek.tokarz14@gmail.com>
2025-07-17 10:32:49 +0200

    Constructor inheritance and base class constructor calling

--- 07_Inheritance/BasicsOfInheritance/Program.cs
+++ 07_Inheritance/BasicsOfInheritance/Program.cs
@@ -21,11 +21,21 @@
             derivedClass.ShowFields();
             */
 
+            /*
             Dog myDog = new Dog();
             myDog.MakeSound();
 
             Cat myCat = new Cat();
             myCat.MakeSound();
+            */
+
+            /*
+            Employee joe = new Employee("Joe", 36);
+            joe.DisplayPersonInfo();
+            */
+
+            Employee joe = new Employee("Joe", 36, "Sales Rep", 12345);
+            joe.DisplayEmployeeInfo();
 
             Console.ReadKey();
         }
@@ -104,6 +114,51 @@
             protectedField = 2;
             // CAUSES AN ERROR
             // privateField = 3;
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        // Base class constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+            Console.WriteLine("Person constructor called");
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Age {Age}");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public string JobTitle { get; private set; }
+
+        public int EmployeeId { get; private set; }
+
+        // Derived class constructor
+        public Employee(string name, int age, string jobTitle, int employeeId)
+            : base(name, age)
+        {
+            JobTitle = jobTitle;
+            EmployeeId = employeeId;
+            Console.WriteLine("Employee (derived class) constructor called");
+            JobTitle = jobTitle;
+            EmployeeId = employeeId;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            DisplayPersonInfo();
+            Console.WriteLine($"Job Title: {JobTitle}, EmployeeID: {EmployeeId} ");
         }
     }
 }
