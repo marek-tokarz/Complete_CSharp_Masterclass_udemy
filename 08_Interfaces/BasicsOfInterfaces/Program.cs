c6194e35441ca19ce8084136cafb33ee8e18cc3a
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-21 20:34:31 +0200

    Simple interface and two classes that implement this

---
+++ 08_Interfaces/BasicsOfInterfaces/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace BasicsOfInterfaces
+{
+    public interface IAnimal
+    {
+        // no implementation of a method
+        void MakeSound();
+
+        void Eat(string food);
+    }
+
+    public class Dog : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Dog ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Cat ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.MakeSound();
+            dog.Eat("Treat");
+
+            Cat cat = new Cat();
+            cat.MakeSound();
+            cat.Eat("Fish");
+
+            Console.ReadKey();
+        }
+    }
+}
