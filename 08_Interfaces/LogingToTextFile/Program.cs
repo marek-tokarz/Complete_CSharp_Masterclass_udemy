ab7470cdba52f844bce3e4087c62c43bdd2f9796
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-25 17:23:47 +0200

    Decoupling a code by using an interface and an implementation of interface in a specific classes, then the application is independent of sepcific implementation of given interface

--- 08_Interfaces/LogingToTextFile/Program.cs
+++ 08_Interfaces/LogingToTextFile/Program.cs
@@ -1,19 +1,66 @@
 ﻿namespace LogingToTextFile
 {
-    internal class Program
+    public interface ILogger
     {
-        static void Main(string[] args)
+        void Log(string message);
+    }
+
+    public class FileLogger : ILogger
+    {
+        public void Log(string message)
         {
             string directoryPath = @"C:\Logs";
             string filePath = Path.Combine(directoryPath, "log.txt");
-            string message = "This is a log entry";
 
-            if(!Directory.Exists(directoryPath))
+            if (!Directory.Exists(directoryPath))
             {
                 Directory.CreateDirectory(directoryPath);
             }
 
             File.AppendAllText(filePath, message + "\n");
+        }
+    }
+
+    public class DatabaseLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            // Implement the logic to log a message to a database
+            Console.WriteLine($"Logging to the database. {message}");
+        }
+    }
+
+    public class Application
+    {
+        private readonly ILogger _logger;
+
+        public Application(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoWork()
+        {
+            _logger.Log("Work started");
+            // DO THE WORK
+            _logger.Log("WORK DONE!");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ILogger fileLogger = new FileLogger();
+            Application app = new Application(fileLogger);
+            app.DoWork();
+
+            ILogger dbLogger = new DatabaseLogger();
+            app = new Application(dbLogger);
+            app.DoWork();
+
             Console.ReadKey();
         }
     }
