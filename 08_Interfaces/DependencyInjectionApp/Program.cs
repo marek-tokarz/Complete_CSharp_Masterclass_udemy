08ef92129e391bc0f59abf1d03dc51446c6e4552
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-25 18:49:43 +0200

    First version - a class Builder has dependencies in itself - it creates its own dependencies by itself - co class Builder is responsible for creating those dependencies

---
+++ 08_Interfaces/DependencyInjectionApp/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DependencyInjectionApp
+{
+    public class Hammer
+    {
+        public void Use()
+        {
+            Console.WriteLine("Hammering nails");
+        }
+    }
+
+    public class Saw
+    {
+        public void Use()
+        {
+            Console.WriteLine("Sawing a wood");
+        }
+    }
+    public class Builder
+    {
+        private Hammer _hammer;
+        private Saw _saw;
+
+        public Builder()
+        {
+                _hammer = new Hammer(); // Builder is responsible for
+                                        // creating its dependencies
+                _saw = new Saw();
+        }
+
+        public void BuildHous()
+        {
+            _hammer.Use();
+            Console.WriteLine("The house is built");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
