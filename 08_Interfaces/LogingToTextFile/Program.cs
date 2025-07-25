3d6e5067ea37eb93cc378a245a5bc552bf4e987e
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-25 16:19:43 +0200

    Creating a directory path and a file path, then creating a folder and then appending message to the text file

---
+++ 08_Interfaces/LogingToTextFile/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LogingToTextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Logs";
+            string filePath = Path.Combine(directoryPath, "log.txt");
+            string message = "This is a log entry";
+
+            if(!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.AppendAllText(filePath, message + "\n");
+            Console.ReadKey();
+        }
+    }
+}
