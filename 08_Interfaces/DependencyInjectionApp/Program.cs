2d8ab0b7875c8003183b31b15bb8697cf1bafe6b
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-27 09:57:14 +0200

    Two types of Dependency Injection (DI): Constructor DI (dependencies passed to the constructor) and Setter DI (dependencies passed by setters)

--- 08_Interfaces/DependencyInjectionApp/Program.cs
+++ 08_Interfaces/DependencyInjectionApp/Program.cs
@@ -17,19 +17,35 @@
     }
     public class Builder
     {
+
+        public Hammer Hammer {get; set; }
+        public Saw Saw { get; set; }
+
+        /*
         private Hammer _hammer;
         private Saw _saw;
+        */
 
-        public Builder()
+        // Setter DI - a default constructor is used instead of custom constructor
+
+        /*
+        public Builder(Hammer hammer, Saw saw)
         {
-                _hammer = new Hammer(); // Builder is responsible for
-                                        // creating its dependencies
-                _saw = new Saw();
+            _hammer = hammer; // Constructor Dependency Injection (DI)
+            _saw = saw;
+            
+            
+               // _hammer = new Hammer(); // Builder is responsible for
+                                          // creating its dependencies
+               // _saw = new Saw();
+            
         }
+        */
 
-        public void BuildHous()
+        public void BuildHouse()
         {
-            _hammer.Use();
+            Hammer.Use();
+            Saw.Use();
             Console.WriteLine("The house is built");
         }
     }
@@ -38,7 +54,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Hammer hammer = new Hammer();
+            Saw saw = new Saw();
+            Builder builder = new Builder();
+            builder.Hammer = hammer;
+            builder.Saw = saw;
+
+            builder.BuildHouse();
+            
+            Console.Read();
         }
     }
 }
