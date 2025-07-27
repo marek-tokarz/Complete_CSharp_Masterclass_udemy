7146bff001f349505e054d6c81eb94b05ea91f3c
marek-tokarz <marek.tokarz14@gmail.com>
2025-07-27 14:38:57 +0200

    Simple program presenting inheritance from two interfaces by one class

---
+++ 08_Interfaces/DIMultipleInheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DIMultipleInheritance
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+
+    public interface IScannable
+    {
+        void Scan();
+    }
+
+    public class MultiFunctionPrinter(): IPrintable, IScannable
+    {
+
+        public void Print()
+        {
+            Console.WriteLine("Printing a document");
+        }
+
+        public void Scan()
+        {
+            Console.WriteLine("Scanning a document");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintable printable = new MultiFunctionPrinter();
+            IScannable scannable = new MultiFunctionPrinter();
+
+            MultiFunctionPrinter printer = new MultiFunctionPrinter();
+
+            printer.Print();
+            printer.Scan();
+
+            Console.ReadKey();
+        }
+    }
+}
