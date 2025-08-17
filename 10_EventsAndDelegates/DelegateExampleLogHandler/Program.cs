a973ea130aecd79e3ea73f4aad788585ca58c18c
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-17 10:32:42 +0200

    Simple example of delegates with assigning a two methods one after another to the delegate (overwriting for the second method)

---
+++ 10_EventsAndDelegates/DelegateExampleLogHandler/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DelegateExampleLogHandler
+{
+    public delegate void LogHandler(string message);
+
+    public class Logger
+    {
+        public void LogToConsole(string message)
+        {
+            Console.WriteLine("Console Log: " + message);
+        }
+
+        public void LogToFile(string message)
+        {
+            Console.WriteLine("File log: " + message);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // class Logger
+            Logger logger = new Logger();
+            
+            // delegate LogHandler - assigning a lmethod from a class Logger
+            LogHandler logHandler = logger.LogToConsole;
+            logHandler("Logging to console");
+
+            // overriding a logHandler with another method
+            logHandler = logger.LogToFile;
+            logHandler("Logging to file");
+
+            Console.ReadKey();
+        }
+    }
+}
