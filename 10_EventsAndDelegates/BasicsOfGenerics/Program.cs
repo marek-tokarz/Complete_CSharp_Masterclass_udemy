c59f648dcd923eeb73f74475c527fcbc0c65fac3
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-17 11:03:42 +0200

    Simple example of a generic method that prints array of elements of any type

---
+++ 10_EventsAndDelegates/BasicsOfGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BasicsOfGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            string[] stringArray = { "One", "Two", "Three", "Four" };
+
+            PrintArray(intArray);
+            PrintArray(stringArray);
+
+            Console.ReadLine();
+        }
+
+        // a generci Method, that accepts a generic datatype
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (T item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
