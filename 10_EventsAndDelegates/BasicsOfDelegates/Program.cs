93b7605f1c4ff1c13b7224bc748e54bfbb3858ab
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-17 10:10:59 +0200

    Simple example of declaration, instatiation and invocation of a specific delegate

---
+++ 10_EventsAndDelegates/BasicsOfDelegates/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BasicsOfDelegates
+{
+    internal class Program
+    {
+        // 1. Declaration
+        public delegate void Notify(string message);
+        // no body for a delegate
+
+        static void Main(string[] args)
+        {
+            // Delegates define a method signature,
+            // and any method assigned to a delegate must match this signature.
+
+            // 2. Instantiation:
+            // Notify notifyDelegate = ShowMessage;
+            Notify notifyDelegate = new Notify(ShowMessage);
+
+            // 3. Invocation
+            notifyDelegate("Hello, Delegates!");
+
+            Console.ReadKey();
+        }
+
+        static void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
