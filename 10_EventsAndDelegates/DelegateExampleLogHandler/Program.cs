47a7ed535f11a0f3ef62ede2d523b3ff1248d3e3
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-19 17:04:29 +0200

    Using method: GetInvocationList() to loop methods from a specific delegate, safely removing a method from a delegate and invoking a method from a delegate in a safe way - by checking if it is not null.

--- 10_EventsAndDelegates/DelegateExampleLogHandler/Program.cs
+++ 10_EventsAndDelegates/DelegateExampleLogHandler/Program.cs
@@ -22,7 +22,7 @@
             // class Logger
             Logger logger = new Logger();
             
-            // delegate LogHandler - assigning a lmethod from a class Logger
+            // delegate LogHandler - assigning a method from a class Logger
             LogHandler logHandler = logger.LogToConsole;
             /*
             logHandler("Logging to console");
@@ -35,13 +35,66 @@
             // Multicast delegate
             logHandler += logger.LogToFile;
 
+            // invoking the multicast delegate
             logHandler("Log this info");
 
-            logHandler -= logger.LogToFile;
+            foreach (LogHandler handler in logHandler.GetInvocationList())
+            {
+                try
+                {
+                    handler("Event occured with error handling");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Exception caught: " + ex.Message);
+                }
 
-            logHandler("After removing a logToFile");
+            }
+
+            // Removing a method from a delegate
+            if(IsMethodInDelegate(logHandler, logger.LogToFile))
+            {
+                logHandler -= logger.LogToFile;
+                Console.WriteLine("LogToFile method removed");
+            }else
+            {
+                Console.WriteLine("LogToFile method not found");
+            }
+
+            if(logHandler != null)
+            {
+                // logHandler("After removing a logToFile");
+                InvokeSafely(logHandler, "After removing a logToFile");
+            }
 
             Console.ReadKey();
+        }
+
+        static void InvokeSafely(LogHandler logHandler, string message)
+        {
+            LogHandler tempLogHandler = logHandler;
+            if (tempLogHandler != null)
+            {
+                tempLogHandler(message);
+            }
+        }
+
+        static bool IsMethodInDelegate(LogHandler logHandler, LogHandler method)
+        {
+            if(logHandler == null)
+            {
+                return false;
+            }
+
+            foreach(var d in logHandler.GetInvocationList())
+            {
+                if(d == (Delegate)method)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
