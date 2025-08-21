ddba10e9f269ce20789edbd9762d08d85afe9d14
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-21 17:12:17 +0200

    Simple project about events - basic example

---
+++ 10_EventsAndDelegates/IntroToEvents/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace IntroToEvents
+{
+    public delegate void Notify(string message);
+
+    public class EventPublisher
+    {
+        // The "On" prefix makes it immediately clear that the
+        // method is associated with an event.
+        // It signifies that the method is not just a regular method but
+        // a one that is called when a specific event occurs.
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            // Invoke the event only if there are any subscribers
+            OnNotify?.Invoke(message);
+        }
+    }
+
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            Console.WriteLine("Event received: " + message);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+            publisher.OnNotify += subscriber.OnEventRaised;
+
+            publisher.RaiseEvent("test");
+
+            Console.ReadKey();
+        }
+    }
+}
