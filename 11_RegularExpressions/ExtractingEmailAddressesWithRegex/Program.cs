cc33412b642114ea28e385275b29886e6d0142f9
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-22 19:06:12 +0200

    Project about extraxting email addreses with regex pattern and then printing addresses that were found

---
+++ 11_RegularExpressions/ExtractingEmailAddressesWithRegex/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ExtractingEmailAddressesWithRegex
+{
+    public class Exercise
+    {
+        public void ExtractPatterns(string input)
+        {
+            string pattern = @"\w+@\w+\.\w+";
+            
+            // Implement the method to extract and print email addresses using regex
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matchCollection = regex.Matches(input);
+
+            Console.WriteLine("{0} hits found:\n {1}", matchCollection.Count, input);
+
+            foreach (Match hit in matchCollection)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise exercise = new Exercise();
+
+            string testInput = "Contact us at support@example.com or sales@example.org.1234";
+            exercise.ExtractPatterns(testInput);
+
+            Console.ReadKey();
+        }
+    }
+}
