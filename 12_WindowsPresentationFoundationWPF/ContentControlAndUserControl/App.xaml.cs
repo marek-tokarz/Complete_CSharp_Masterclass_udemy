cb4ddb3b50f9d793cd99a64650c46467585c1dad
marek-tokarz <marek.tokarz14@gmail.com>
2025-08-30 09:00:49 +0200

    Project with login functionality made with ContentControl and UserControl

---
+++ 12_WindowsPresentationFoundationWPF/ContentControlAndUserControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ContentControlAndUserControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
