80f58753d1aac841e123d677f6af45ce684da54b
marek-tokarz <marek.tokarz14@gmail.com>
2025-09-04 19:00:06 +0200

    Created a GUI for logging functionality

--- 12_WindowsPresentationFoundationWPF/InvoiceManagement/MainWindow.xaml.cs
+++ 12_WindowsPresentationFoundationWPF/InvoiceManagement/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace InvoiceManagement
         public MainWindow()
         {
             InitializeComponent();
-            MainContent = new LoginView();
+            MainContent.Content = new LoginView();
         }
     }
 }
