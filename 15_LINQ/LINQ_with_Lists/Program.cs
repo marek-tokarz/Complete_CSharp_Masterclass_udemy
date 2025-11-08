682790ff8c4875d4aa8d10752078bf33cfbc7724
marek-tokarz <marek.tokarz14@gmail.com>
2025-11-08 15:31:23 +0100

    Initial commit of a University Manager

---
+++ 15_LINQ/LINQ_with_Lists/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_with_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        public UniversityManager()
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+
+            universities.Add(new University() { Id = 1, Name = "Yale" });
+            universities.Add(new University() { Id = 2, Name = "Beijing Tech" });
+
+            students.Add(new Student { Id = 1, Name = "Carla", Gender = "female", Age = 17, UniversityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Toni", Gender = "male", Age = 21, UniversityId = 1 });
+            students.Add(new Student { Id = 3, Name = "Leyla", Gender = "female", Age = 19, UniversityId = 2 });
+            students.Add(new Student { Id = 4, Name = "James", Gender = "female", Age = 25, UniversityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Linda", Gender = "female", Age = 22, UniversityId = 2 });
+
+        }
+    }
+
+    class University
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public void Print()
+        {
+            Console.WriteLine("University {0} with ID {1}", Name, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        // Foreign Key
+        public int UniversityId { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("Student {0} with Id {1}, Gender {2} " +
+                "and Age {3} from University with the Id {4));", Name, Id, Gender, Age, UniversityId );
+        }
+    }
+}
