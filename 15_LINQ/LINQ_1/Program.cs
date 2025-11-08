f9d0b1c93308238c3753a6954bfb43af62b477e7
marek-tokarz <marek.tokarz14@gmail.com>
2025-11-08 13:37:01 +0100

    Simple example with LINQ and array of integers

---
+++ 15_LINQ/LINQ_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            OddNumbers(numbers);
+
+            Console.ReadKey();
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Numbers: ");
+
+            IEnumerable<int> oddNumbers = from number in numbers
+                                          where number % 2 != 0
+                                          select number;
+            Console.WriteLine(oddNumbers);
+
+            foreach (int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
