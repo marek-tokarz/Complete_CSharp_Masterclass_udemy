967fa1883d17795e350b720e0f1014407bc15a91
marek-tokarz <marek.tokarz14@gmail.com>
2025-11-09 10:28:07 +0100

    Added two methods in a UniversityManager class and run them

--- 15_LINQ/LINQ_with_Lists/Program.cs
+++ 15_LINQ/LINQ_with_Lists/Program.cs
@@ -10,7 +10,13 @@ namespace LINQ_with_Lists
     {
         static void Main(string[] args)
         {
+            UniversityManager um = new UniversityManager();
 
+            um.MaleStudents();
+
+            um.FemaleStudents();
+
+            Console.ReadKey();
         }
     }
 
@@ -29,10 +35,36 @@ namespace LINQ_with_Lists
 
             students.Add(new Student { Id = 1, Name = "Carla", Gender = "female", Age = 17, UniversityId = 1 });
             students.Add(new Student { Id = 2, Name = "Toni", Gender = "male", Age = 21, UniversityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Frank", Gender = "male", Age = 21, UniversityId = 2 });
             students.Add(new Student { Id = 3, Name = "Leyla", Gender = "female", Age = 19, UniversityId = 2 });
-            students.Add(new Student { Id = 4, Name = "James", Gender = "female", Age = 25, UniversityId = 2 });
+            students.Add(new Student { Id = 4, Name = "James", Gender = "male", Age = 25, UniversityId = 2 });
             students.Add(new Student { Id = 5, Name = "Linda", Gender = "female", Age = 22, UniversityId = 2 });
+        }
 
+        public void MaleStudents()
+        {
+            IEnumerable<Student> maleStudents = from student in students 
+                                                where student.Gender == "male" 
+                                                select student;
+            Console.WriteLine("Male - Students: ");
+
+            foreach(Student student in maleStudents)
+            {
+                student.Print();
+            }
+        }
+
+        public void FemaleStudents()
+        {
+            IEnumerable<Student> femaleStudents = from student in students
+                                                where student.Gender == "female"
+                                                select student;
+            Console.WriteLine("Female - Students: ");
+
+            foreach (Student student in femaleStudents)
+            {
+                student.Print();
+            }
         }
     }
 
@@ -64,7 +96,7 @@ namespace LINQ_with_Lists
         public void Print()
         {
             Console.WriteLine("Student {0} with Id {1}, Gender {2} " +
-                "and Age {3} from University with the Id {4));", Name, Id, Gender, Age, UniversityId );
+                "and Age {3} from University with the Id {4}", Name, Id, Gender, Age, UniversityId );
         }
     }
 }
